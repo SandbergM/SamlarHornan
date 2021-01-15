36cd9b9c6b3d2665d5f083a16a9ba54bd51fe449
SandbergM <56336779+SandbergM@users.noreply.github.com>
2021-01-15 11:50:34 +0100

    Delete index.js

--- backend/config/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  PORT: 4001,
-  SECRET:
-    "NwpSXP4c60A68RBA7wBkJgEknoKLj9qGSFNXfI9SrsFlkuPgdDZx5vffwfzezFpuUPpealMfCgSzfPLzfMyd0ZtMpY2Z2fcYcNQe",
-  SALT:
-    "7FB7F50qhF1kqdFi7CAeGYh9mufuPoJzHS0wWDoIA7FokxpBDk9bhs7yIytfnbxDCDv3Z3GJsKOl3zHQyAOp0lRGDrE4vUezofzg",
-};
